b4b08e0666bded206ff6e539a4e6ab65fdf3999d
nguyenquocp903-web <nguyenquocp903@gmail.com>
2026-04-15 21:50:43 +0700

    Update MayTinhDungStack.Designer.cs

--- doancuoiki/MayTinhDungStack.Designer.cs
+++ doancuoiki/MayTinhDungStack.Designer.cs
@@ -35,8 +35,8 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "MayTinhDungStack";
+            this.Text = "MayTinhDungStack";
 
             this.ResumeLayout(false);
 
